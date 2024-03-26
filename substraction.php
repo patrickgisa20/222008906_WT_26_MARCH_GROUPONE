903cf0355235bf22aa0e75fb7fc70cbb69474d10
patrickgisa20 <gisapatrick20@gmail.com>
2024-03-26 17:34:04 +0200

    first commit

---
+++ substraction.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>add</title>
+</head>
+<body>
+<?php
+if ($_SERVER["REQUEST_METHOD"]=="POST") {
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$full = $fname." ".$lname;
+	$num1 = $_POST['num1'];
+	$num2 = $_POST['num2'];
+	$substraction = $num1 -$num2;
+	echo"<p>hello $full</p>";
+	echo "<p>the substraction of $num1 and $num2 is: $substraction</p>";
+}
+
+?>
+</body>
+</html>
